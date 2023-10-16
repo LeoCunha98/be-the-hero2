4c43ce442d5073d8309772cbbe89feb8923ddf80
Mazilao <gabrielmazilao@gmail.com>
2023-10-16 16:28:57 -0300

    feat: tela de detalhes do incidente

--- routes.js
+++ routes.js
@@ -5,12 +5,14 @@ import { NavigationContainer } from '@react-navigation/native';
 const AppStack = createNativeStackNavigator();
 
 import Incidents from './src/pages/incidentes/index';
+import Detail from './src/pages/detail/index';
 
 export default function Routes() {
   return (
     <NavigationContainer>
       <AppStack.Navigator screenOptions={{ headerShown: false }}>
         <AppStack.Screen name='Incidentes' component={Incidents} />
+        <AppStack.Screen name='Detail' component={Detail} />
       </AppStack.Navigator>
     </NavigationContainer>
   );
