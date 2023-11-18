8607eff129c26b93844f58d27256d4969ecb9552
Mazilao <gabrielmazilao@gmail.com>
2023-11-17 23:37:46 -0300

    tela de login e implementacao do firebase

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
+//import './src/config/firebase';
+import React from 'react';
 import { StyleSheet } from 'react-native';
-import Routes from './routes';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Incidents from './src/pages/incidentes/index'
+import Detail from './src/pages/detail/index'
+import History from './src/pages/history/index'
+import LoginScreen from './src/pages/login';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <Routes />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen options={{ headerShown: false }} name='Login' component={LoginScreen} />
+        <Stack.Screen name='Incidentes' component={Incidents} />
+        <Stack.Screen name='Detail' component={Detail} />
+        <Stack.Screen name='History' component={History} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
---
+++ src/config/firebase.js
@@ -0,0 +1,36 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAbsxnNr0kA9iryG1mjSQUYr6zvYyEL4k0",
+  authDomain: "be-the-hero-55721.firebaseapp.com",
+  projectId: "be-the-hero-55721",
+  storageBucket: "be-the-hero-55721.appspot.com",
+  messagingSenderId: "324293160823",
+  appId: "1:324293160823:web:38a30609eefb9902d73b72",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+
+
+
+
+// let app;
+// if (firebase.apps.length === 0) {
+//   app = firebase.initializeApp(firebaseConfig);
+// } else {
+//   app = firebase.app()
+// }
+// const auth = firebase.auth()
+
+
+export { auth };
